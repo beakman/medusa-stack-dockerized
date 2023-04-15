d32958432d6ed60a3fadd467ad393b9391a16e28
Paco <psalido@gmail.com>
2023-04-15 22:05:36 +0200

    Deactivate event and cache services

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -105,7 +105,7 @@ const plugins = [
 ];
 
 const modules = {
-  eventBus: {
+  /*eventBus: {
     resolve: "@medusajs/event-bus-redis",
     options: {
       redisUrl: REDIS_URL
@@ -116,7 +116,7 @@ const modules = {
     options: {
       redisUrl: REDIS_URL
     }
-  },
+  },*/
 }
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
@@ -128,7 +128,7 @@ const projectConfig = {
   store_cors: STORE_CORS,
   admin_cors: ADMIN_CORS,
   // Uncomment the following lines to enable REDIS
-  redis_url: REDIS_URL
+  // redis_url = REDIS_URL
 }
 
 if (DATABASE_URL && DATABASE_TYPE === "postgres") {
